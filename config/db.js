abd398fe0c0b83d945ae6079e0b60926ec5d60c6
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-07-10 11:32:47 +0600

    implement user create delete update functionality that also perform this operation in mongodb

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async() =>{
     try {
         mongoose.connection.on('connected',()=> console.log('Database Connected'));
-        await mongoose.connect(`${process.env.DATABASE_URL}/cineswift`)
+        await mongoose.connect(`${process.env.DATABASE_URL}`)
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
